8d85212de0da75847dda8a70931e7ff487f9622a
Xavier <41271402+xarenas107@users.noreply.github.com>
2024-02-25 23:33:26 -0500

    fix: set secret key type as string

--- src/types.ts
+++ src/types.ts
@@ -2,14 +2,13 @@ import type { StorageMounts } from 'nitropack'
 import type { NuxtError } from 'nuxt/app'
 import type { EncryptAlgorithm } from './runtime/server/utils/uncsrf'
 import type { HTTPMethod } from 'h3'
-import type { CipherKey } from 'crypto'
 
 type Storage = StorageMounts[string] | keyof StorageMounts[string]
 
 export interface ModuleOptions {
   ttl?:number
   cookieKey?: string
-  secret?:CipherKey
+  secret?:string
   encrypt:{
     secret?: Buffer
     algorithm?: EncryptAlgorithm
