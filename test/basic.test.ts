e541cc6d845d06de743fcffd10cb91e9c4b1aebb
Xavier <41271402+xarenas107@users.noreply.github.com>
2024-06-20 10:29:39 -0500

    fix(test): Fix token response test

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -2,14 +2,22 @@ import { describe, it, expect } from 'vitest'
 import { fileURLToPath } from 'node:url'
 import { setup, $fetch } from '@nuxt/test-utils/e2e'
 
-describe('ssr', async () => {
+describe('csrf protection', async () => {
   await setup({
     rootDir: fileURLToPath(new URL('./fixtures/basic', import.meta.url)),
+    dev: true,
+    nuxtConfig: {
+      test:true
+    }
   })
 
-  it('renders the index page', async () => {
-    // Get response to a server-rendered page with `$fetch`.
-    const html = await $fetch('/')
-    expect(html).toContain('<div>basic</div>')
+  it('token recieved', async () => {
+    await $fetch('/',{
+      onResponse: ({ response }) => {
+        const cookies = response.headers.get('set-cookie')
+        expect(cookies).contain('x-csrf-token')
+      }
+    })
   })
+
 })
