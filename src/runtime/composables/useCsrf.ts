b556a63ca3d3d180ee63767aa63e31e05b4cb74d
Xavier <41271402+xarenas107@users.noreply.github.com>
2024-08-29 16:39:13 -0500

    fix: Solve `ssr` missing value on server side

--- src/runtime/composables/useCsrf.ts
+++ src/runtime/composables/useCsrf.ts
@@ -1,18 +1,9 @@
-import { useNuxtApp, useCookie, useRuntimeConfig } from '#app'
-import { getCookie } from 'h3'
-import type { H3Event } from 'h3'
+import { useCookie, useRuntimeConfig } from '#app'
 
 export function useCsrf() {
   const runtime = useRuntimeConfig()
+  const { cookie } = runtime.public.uncsrf ?? {}
 
-  if (import.meta.server) {
-		const app = useNuxtApp()
-		const event = app.ssrContext?.event as H3Event
-		const cookie = event ? getCookie(event,runtime.uncsrf?.cookie?.name) : null
-		const csrf = cookie || null
-    return { csrf }
-  }
-
-	const token = useCookie<string>(runtime.public.uncsrf?.cookie?.name)
-  return { csrf:token.value || null }
+	const token = useCookie<string>(cookie?.name)
+  return { csrf: token.value || null }
 }
